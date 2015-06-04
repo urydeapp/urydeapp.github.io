2c17f950be0073fb789a520b6b6539b9533e84ca
psdewar2 <psdewar2@live.com>
2015-06-04 00:18:28 -0400

    Added signup mechanism
    Updated signup mechanism with username and password

--- assets/js/parse.js
+++ assets/js/parse.js
@@ -2,14 +2,29 @@ Parse.initialize("XtZNXXbzP5R99gWlwRM6ZiRjXxrRoKRd8UEy5QoU", "u2GXAp1rs6x7MYKUtL
 
 //parse.js
 
-//test object
-var TestObject = Parse.Object.extend("TestObject");
-var testObject = new TestObject();
-testObject.save({foo: "bar"}, {
-	success: function() {
-		alert("yay! it worked");
-	},
-	error: function(error) {
-		console.log("Didn't work");
-	}
-});
+//extend any object that's not user this way
+//var User = Parse.Object.extend("User");
+
+//otherwise
+var user = new Parse.User();
+function getUser(e) {
+	user.set("username", document.getElementById('username').value);
+	user.set("password", document.getElementById('password').value);
+	user.set("email", document.getElementById('userEmail').value);
+	user.signUp(null, {
+		success: function(user) {
+	    	// Hooray! Let them use the app now.
+			alert("Yay it worked");
+		},
+		error: function(user, error) {
+	    	// Show the error message somewhere and let the user try again.
+	    	alert("Error: " + error.code + " " + error.message);
+	 	}
+	});
+
+	e.preventDefault();
+}
+/*
+user.set("firstName", "PSD");
+user.set("lastName", "isDope");
+*/
