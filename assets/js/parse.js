7a26d910077c7a7d5273bae188d06e2da6ce1bb3
psdewar2 <psdewar2@live.com>
2015-06-12 04:49:18 -0400

    Implemented user verification mechanism

--- assets/js/parse.js
+++ assets/js/parse.js
@@ -16,41 +16,85 @@ var email = document.getElementById('userEmail');
 var school = document.getElementById('school');
 var phoneNum = document.getElementById('phoneNum');
 
-
-
 function checkUserInfo() {
-    if(username.value.length < 6){
-        username.style.color = "#FF0000";
-        return false;
-    } else {
-        username.style.color = "#00FF00";
+    if (firstName.value.length > 1) {
+        firstName.style.borderColor = "#00FF00";
+    }
+    if (lastName.value.length > 1) {
+        lastName.style.borderColor = "#00FF00";
+    }
+    if(username.value.length >= 6 && username.value.length <= 20) {
+        username.style.borderColor = "#00FF00";
+    }
+    if(password.value.length >= 6 && password.value.length <= 20 && password.value === confPass.value) {
+        password.style.borderColor = "#00FF00";
+        confPass.style.borderColor = "#00FF00";
+    }
+    if (isCorrect(email.value)) {
+        email.style.borderColor = "#00FF00";
+    }
+    if (school.value.length !== 0) {
+        school.style.borderColor = "#00FF00";
+    }
+    if (phoneNum.value.length == 10 || phoneNum.value.length == 11) {
+        phoneNum.style.borderColor = "#00FF00";
     }
 
-    if (password.value.length < 6 || confPass.value.length < 6 || password.value !== confPass.value) {
-        password.style.color = "#FF0000";
-        confPass.style.color = "#FF0000";
-        return false;
-    } else {
-        password.style.color = "#00FF00";
-        confPass.style.color = "#00FF00";
-    }
 }
 
+function alertUser() {
+    this.render = function (dialog) {
+        var winW = window.innerWidth;
+        var winH = window.innerHeight;
+        var dialogoverlay = document.getElementById('dialogoverlay');
+        var dialogbox = document.getElementById('dialogbox');
+        dialogoverlay.style.display = "block";
+        dialogoverlay.style.height = winH + "px";
+        dialogbox.style.top = "100px";
+        dialogbox.style.display = "block";
+        document.getElementById('dialogboxbody').innerHTML = dialog;
+        document.getElementById('dialogboxfoot').innerHTML = '<button class="btn-lg btn-theme" onclick="Alert.ok()">OK</button>';
+    }
+    this.ok = function () {
+        if (document.getElementById('dialogboxbody').innerHTML == firstName.value + ", thank you for joining uRyde!") {
+            $('#myModal').modal('hide'); //hides modal - not necessary if refreshing correctly
+            window.location.reload(); //refreshes page
+        }
+        document.getElementById('dialogbox').style.display = "none";
+        document.getElementById('dialogoverlay').style.display = "none";
+
+    }
+}
+var Alert = new alertUser();
+
 function setUser() {
-
-    var signupButton = document.getElementById('join');
-
     //QA stuff
-    /*
-        if (username.validity.tooLong || username.value.length < 6) {
-            username.setCustomValidity("Username must be between 6 and 20 characters");
-        }
-        else {
-        }
-    */
-
-
-    if (checkUserInfo) {
+    if (firstName.value.length <= 1) {
+        firstName.style.borderColor = "#FF0000";
+        Alert.render('Please fill out your first name correctly.');
+    } else if (lastName.value.length <= 1) {
+        lastName.style.borderColor = "#FF0000";
+        Alert.render('Please fill out your last name correctly.');
+    } else if(username.value.length < 6 || username.value.length > 20){
+        username.style.borderColor = "#FF0000";
+        Alert.render('Username must be between 6 and 20 characters.');
+    } else if (password.value.length < 6 || password.value.length > 20) {
+        password.style.borderColor = "#FF0000";
+        Alert.render('Password must be between 6 and 20 characters.');
+    } else if (password.value !== confPass.value) {
+        password.style.borderColor = "#FF0000";
+        confPass.style.borderColor = "#FF0000";
+        Alert.render('Please re-enter your password.');
+    } else if (!isCorrect(email.value)) {
+        email.style.borderColor = "#FF0000";
+        Alert.render('Please use a valid email ending with edu if you have one.');
+    } else if (school.value.length == 0) {
+        school.style.borderColor = "#FF0000";
+        Alert.render('Please enter the school you attend.');
+    } else if (phoneNum.value.length !== 10 && phoneNum.value.length !== 11) {
+        phoneNum.style.borderColor = "#FF0000";
+        Alert.render('Please enter your phone number correctly.');
+    } else {
         user.set("firstName", firstName.value);
         user.set("lastName", lastName.value);
         user.set("username", username.value);
@@ -59,14 +103,11 @@ function setUser() {
         user.set("school", school.value);
         user.set("phoneNum", phoneNum.value);
     }
-    
 
 	user.signUp(null, {
 		success: function(user) {
 	    	// Hooray! Let them use the app now.
-			alert(firstName.value + ", thank you for joining uRyde!");
-            $('#myModal').modal('hide'); //hides modal - not necessary if refreshing correctly
-            window.location.reload(); //refreshes page
+			Alert.render(firstName.value + ", thank you for joining uRyde!");
             /*
             signupButton.value = "Welcome!";
             signupButton.style.color = "#FFD700";
@@ -74,9 +115,18 @@ function setUser() {
 		},
 		error: function(user, error) {
 	    	// Show the error message somewhere and let the user try again.
-	    	alert("Error: " + error.code + " " + error.message);
+            //if (error.code !== 200 || error.code !== 201) {
+            //Alert.render("Error: " + error.code + " " + error.message);
+
+
 	 	}
 	});
 
+}
+
+function isCorrect(emailInput) {
+    var pattern = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.edu$/;
+    //same as var pattern = new RegExp("");
+    return pattern.test(emailInput);
 }
 
